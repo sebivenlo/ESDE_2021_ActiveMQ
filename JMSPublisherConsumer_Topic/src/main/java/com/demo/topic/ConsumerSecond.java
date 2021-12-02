1e9e5800232a083a6b6a3f3ae3f5067ae77d0ea8
Fotis <f.alatas@student.fontys.nl>
2021-12-02 09:43:03 +0100

    add second consumer

---
+++ JMSPublisherConsumer_Topic/src/main/java/com/demo/topic/ConsumerSecond.java
@@ -0,0 +1,53 @@
+package com.demo.topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.util.Random;
+
+public class ConsumerSecond {
+
+    private static String USER = "User";
+    private static String TIME = "Time";
+    private static String MESSAGE = "Message";
+
+
+    public static void main(String[] args) throws JMSException {
+        ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin",
+                "tcp://localhost:61616");
+        Connection connection = null;
+        Session session = null;
+        MessageConsumer consumer = null;
+        try {
+            Random rand = new Random();
+            int subNumber = rand.nextInt(1000);
+
+            connection = factory.createConnection();
+            connection.setClientID("Consumer-" + subNumber);
+
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Topic topic = session.createTopic("Topic-Name");
+
+            consumer = session.createDurableSubscriber(topic, "Consumer-" + subNumber);
+            consumer.setMessageListener(message -> {
+                try {
+                    System.out.println(message.getObjectProperty(USER) + " " + message.getObjectProperty(TIME));
+                    System.out.println(message.getObjectProperty(MESSAGE));
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            });
+            // star the connection
+            connection.start();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
+            // close resources
+            consumer.close();
+            session.close();
+            connection.close();
+        }
+    }
+
+}
